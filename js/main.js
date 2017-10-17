30aa4cbba7302dc563b4be0df547a6702ecb2eeb
kieranmcclung <kieranmcclung@hotmail.com>
2017-10-17 23:13:02 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@
 		$('#nav a').each(function () {
 			var currLink = $(this);
 			var refElement = $(currLink.attr("href"));
-			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			if (refElement.position().top - 100 <= scrollPos && refElement.position().top + refElement.height() > scrollPos ) {
 				$('#nav a').removeClass("active");
 				currLink.addClass("active");
 			}
